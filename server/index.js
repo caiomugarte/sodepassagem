b2def991bd3e60be3d058f3748691810864e7a8d
Caio Notebook <caio.mugarte@gmail.com>
2022-08-10 17:31:28 -0300

    api começando a funcionar

--- server/index.js
+++ server/index.js
@@ -2,10 +2,35 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
-const app = express()
+const app = express();
+
+const axios = require('axios');
+
+var empresasAreas = "";
+var voos = "";
+var itinerarios = "";
+
+(async () => {
+  try {
+    const response = await axios.get('https://skiplagged.com/api/search.php?from=BSB&to=SSA&depart=2022-10-20&return=&format=v3&counts%5Badults%5D=1&counts%5Bchildren%5D=0')
+    console.log(response.data.airlines);
+    empresasAreas = response.data.airlines;
+    voos = response.data.flights;
+    itinerarios = response.data.itineraries;
+  } catch (error) {
+    console.log(error.response.body);
+  }
+})();
 
 app.get("/api", (request, response) => {
-    response.json({message: "Hello from server!"});
+    response.json({
+        airlines: empresasAreas,
+        voos : voos,
+        message : "teste",
+        itinerarios : itinerarios,
+
+    
+    });
 });
 
 
